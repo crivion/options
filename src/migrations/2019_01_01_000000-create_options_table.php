7cd614fcd8ef75cf6f41417d4eaf81a89784e725
Alex Crivion <crivion@gmail.com>
2020-01-06 18:15:47 +0200

    fixed migration issue

--- src/migrations/2019_01_01_000000-create_options_table.php
+++ src/migrations/2019_01_01_000000-create_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Options extends Migration
+class OptionsTable extends Migration
 {
     /**
      * Run the migrations.
